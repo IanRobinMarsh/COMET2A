44f44e8b8613682f890a1056083258fdf7eecbe8
Ian Marsh <ian.robin.marsh@gmail.com>
2023-11-13 21:24:29 +0100

    simplified

--- main.R
+++ main.R
@@ -20,17 +20,12 @@ require(gsignal, quietly=T, warn.conflicts=F)
 require(R.matlab, quietly=T, warn.conflicts=F)
 require(swfscMisc, quietly=T, warn.conflicts=F)
 require(signal, quietly=T, warn.conflicts=F)
+require(roll, quietly=T, warn.conflicts=F)
 
 source("./read_data.R")
 
 
 
-# 	Gilbert crossing
-#
-# 	source("diffs.R")
-# 	source("gilbert.R")
-# 	source("run_gilbert.R")
-# 	source("plot_gilbert.R")
 
 
 #
@@ -42,31 +37,13 @@ OscTests <- c("heuristics", "crossing", "wavelet", "deeplearning", "fft")
 #
 # 	Choose a random sequence 1-27 (1-20 with Oscillations and 21-28 Without)
 # 
-seed <- 2 # not sure need this
-
-num <- round(runif(n=1, min=1, max=28))
-print(paste("selected sequence", num))
+sequence <-  sample(1:28, 28, replace=F)
+# print(paste("selected sequence", sequence))
 
 
-#
-# 	Import (unknown) signal
-#
-if (num <= 20) {
-	thisSeqSig <- WithOsc$num
-	thisSeqOsc <- WithOsc$num
-	thisSeqTim <- WithOsc$num 
-} else if (num > 20) {
-	thisSeqSig <- WithoutOsc$num
-	thisSeqOsc <- WithoutOsc$num
-	thisSeqTim <- WithoutOsc$num 
-} else {
-	print("bad sequence.")
-}
+diffs(WithOsc, 10, 10)
+diffs(WithoutOsc, 10, 10)
 
-
-MyTest <- function( ) {
-       	  	    
-}
 
 #
 #	Run sequence through implemented tests 
