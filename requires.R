5a76e511047d30a5a081f413f0010aac4bdfa8c1
Ian Marsh <ian.marsh@ri.se>
2023-11-14 11:53:28 +0100

    packages needed

---
+++ requires.R
@@ -0,0 +1,13 @@
+#
+# Stuff we need
+#
+
+# Roll and data.table both have roll, AFAIK, so use, roll::roll()
+
+require(data.table, quietly=T, warn.conflicts=F)
+require(roll, quietly=T, warn.conflicts=F)
+require(dplyr, quietly=T, warn.conflicts=F)
+require(gsignal, quietly=T, warn.conflicts=F)
+require(R.matlab, quietly=T, warn.conflicts=F)
+require(swfscMisc, quietly=T, warn.conflicts=F)
+require(signal, quietly=T, warn.conflicts=F)
